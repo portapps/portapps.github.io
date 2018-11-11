f1767ceaeb6c4f64ae64d0a96974e445a606c3e8
CrazyMax <crazy-max@users.noreply.github.com>
2018-11-11 03:45:57 +0100

    Update travis

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# only process script when started not by pull request
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  echo "This is a PR, exiting..."
-  exit 0
-fi
-
 # enable error reporting to the console
 set -e
-
-# build
-gulp --env=production build
 
 # publish to master branch
 cd ./web
@@ -22,6 +13,3 @@ git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
 rm -rf .git
 cd ../
-
-# test
-#htmlproofer ./web --allow-hash-href --check-favicon --only-4xx --alt-ignore "/.*/" --url-ignore "/#0/" --log-level=:debug
